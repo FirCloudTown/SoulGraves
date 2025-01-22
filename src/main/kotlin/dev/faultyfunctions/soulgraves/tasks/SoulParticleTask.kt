482e09ddffb18370e492751880bb8baa34464dba
iamnot23 <131678378+iamnot23@users.noreply.github.com>
2025-01-22 22:07:18 +0800

    Update SoulParticleTask.kt
    v1.2.2 修改了粒子指向目标的方式

--- src/main/kotlin/dev/faultyfunctions/soulgraves/tasks/SoulParticleTask.kt
+++ src/main/kotlin/dev/faultyfunctions/soulgraves/tasks/SoulParticleTask.kt
@@ -6,27 +6,18 @@ import org.bukkit.Particle
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitRunnable
-import java.util.UUID
+import java.util.*
 
 class SoulParticleTask : BukkitRunnable() {
 
     private var enableParticles = ConfigManager.enableParticles
     private var particle = Particle.SOUL_FIRE_FLAME
-    private var space = ConfigManager.particleSpace
-    private var interval = ConfigManager.particleInterval // 改成同步任务的话任务周期由主类控制了，这个会灰了
-    private var maxAmount = ConfigManager.particleAmount
-    private var duration = ConfigManager.particleDuration
     private var followRadius = ConfigManager.particlesFollowRadius
     val init_distance = 3
-    val min_distanceSquared = 1
 
     val nearbyPlayerFilter: (Entity, UUID) -> Boolean = { entity, uuid ->
         entity is Player && entity.uniqueId == uuid
     }
-
-    var tick: Int = 0
-    var locations: MutableList<org.bukkit.Location> = mutableListOf()
-    var amount = maxAmount // 当粒子距离目标过近时amount会减小以防止粒子穿过目标
 
     override fun run() {
         if (!enableParticles) return
@@ -45,45 +36,22 @@ class SoulParticleTask : BukkitRunnable() {
 
             if (nearbyPlayer == null) continue
 
-            // 创建粒子任务
             // 检查灵魂是否已被拾取或玩家状态是否满足条件
             if (!SoulGraves.soulList.contains(soul) ||
                 !nearbyPlayerFilter(nearbyPlayer, uuid) ||
                 !nearbyPlayer.isOnline ||
                 nearbyPlayer.location.distance(soul.location) > followRadius) {
-                cancel() // 取消粒子任务
                 return
             }
 
-            if (tick == 0) {
-                // 计算粒子轨迹
-                val direction = nearbyPlayer.eyeLocation.direction
-                val origin = nearbyPlayer.eyeLocation.add(direction.multiply(init_distance))
-                val unit = soul.location.clone().toVector().subtract(origin.toVector()).normalize()
+            // 生成粒子
+            val direction = nearbyPlayer.eyeLocation.direction
+            val origin = nearbyPlayer.eyeLocation.add(direction.multiply(init_distance))
+            // 从origin指向soul的向量，不是单位向量
+            val line = soul.location.clone().toVector().subtract(origin.toVector())
+            // 参数：粒子类型，起始位置，粒子数量（为0时为向量模式）， 粒子生成随机偏移xyz（向量模式时为粒子方向），速度，特殊数据，强制显示
+            world.spawnParticle(particle, origin, 0, line.x, line.y,line.z, 0.05, null, true)
 
-                for (i in 1..maxAmount) {
-                    val pos = origin.clone().add(unit.clone().multiply(i * space))
-                    if (pos.distanceSquared(pos) >= min_distanceSquared) {
-                        locations.add(pos)
-                    } else {
-                        // 当粒子距离目标过近时amount会减小以防止粒子穿过目标
-                        amount--
-                    }
-                }
-            }
-
-            // 控制粒子的生成
-            if (tick < amount) {
-                world.spawnParticle(particle, locations[tick], 1, 0.1, 0.1, 0.1, 0.0)
-                tick++
-            } else if (tick < duration + maxAmount) {
-                // 跳过粒子的生成
-                tick++
-            } else if (tick >= duration + maxAmount) {
-                // 重置计时
-                tick = 0
-            }
         }
     }
-}
 }
